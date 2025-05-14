dd49a9cdc22c4ce646774e33717acb4a6e8f4dd3
JoelHuancaZevallos <u20201b914@upc.edu.pe>
2025-05-13 23:27:41 -0500

    feat: implement navigations

---
+++ src/navigation/model/vehicle.entity.js
@@ -0,0 +1,22 @@
+export class Vehicle{
+    constructor( { id= '',
+                     model= '',
+                     brand= '',
+                     year= '',
+                     description= '',
+                     image= '',
+                     price= '',
+                     rating= ''
+                 }) {
+
+        this.id = id;
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.rating = rating;
+
+    }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import {createRouter, createWebHistory} from "vue-router";
 const HomeComponent = () => import('../public/pages/home.component.vue');
 const PageNotFoundComponent = () => import('../public/pages/page-not-found.component.vue');
 const PublicationComponent = () => import('../publications/pages/publication-page.component.vue')
+const VehicleManagementComponent=()=> import('../navigation/pages/vehicle-management.component.vue')
 
 /**
  * @type {import('vue-router').RouteRecordRaw[]}
@@ -27,6 +28,7 @@ const PublicationComponent = () => import('../publications/pages/publication-pag
 const routes = [
     {   path: '/home',                  name: 'home',       component: HomeComponent,               meta: {title: 'Home'}},
     {   path: '/',                      name: 'default',    redirect: {name: 'home'}},
+    {   path: '/navigation', name:'navigation' , component: VehicleManagementComponent,    meta: {title: 'Navigation'}},
     {   path: '/publications',           name: 'publication', component: PublicationComponent,       meta: {title: 'Publications'}},
     {   path: '/:pathMatch(.*)*',       name: 'not-found',  component: PageNotFoundComponent,       meta: {title: 'Page not found'}},
 ]
